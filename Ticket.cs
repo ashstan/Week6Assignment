ef476a350bb04cff8df6e0fe433714f906fa5ab8
Ashley Stanley <astanley5@my.wctc.edu>
2022-02-25 06:17:53 -0600

    Getters and setters for ticket class

--- Ticket.cs
+++ Ticket.cs
@@ -5,6 +5,12 @@ namespace Week6Assignment
 {
     public class Ticket
     {
-        
+            public string ticketID { get; set; }
+            public string summary { get; set; }
+            public string status { get; set; }
+            public string priority { get; set; }
+            public string submitter { get; set; }
+            public string assigned { get; set; }
+            public string watching { get; set; }
     }
 }
