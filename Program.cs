5b5dca613b412913db9d097edd3b4a55674f0300
Ashley Stanley <astanley5@my.wctc.edu>
2022-02-26 07:22:09 -0600

    Code for creating ticketID number

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Week6Assignment
 {
@@ -7,60 +9,82 @@ namespace Week6Assignment
     {
         static void Main(string[] args)
         {
+
+            //commented out, ticket class will create these
+            // string ticketID;
+            // string summary;
+            // string status;
+            // string priority;
+            // string submitter;
+            // string assigned;
+            // string watching;
+
+            //create parallel lists of string details
+            //lists are used since we do not know number of lines of data
             string file = "systemtickets.csv";
             string addTicket;
 
-            string ticketID;
-            string summary;
-            string status;
-            string priority;
-            string submitter;
-            string assigned;
-            string watching;
-            
+             List<string> ticketID = new List<string>();
+             List<string> summary = new List<string>();
+             List<string> status = new List<string>();
+             List<string> priority = new List<string>();
+             List<string> submitter = new List<string>();
+             List<string> assigned = new List<string>();
+             List<string> watching = new List<string>();
+
+
             Console.WriteLine("Ticketing System Application\n");
 
             Console.WriteLine("Would you like to add a ticket? (y/n)");
             addTicket = Console.ReadLine().ToLower();
 
-            // if (addTicket == "y") {
-            //     if (File.Exists(file))
-            //     {
-                    //creating file
-                    StreamWriter sw = new StreamWriter(file);
-                    do {
-                        Console.WriteLine("Enter ticket ID: ");
-                        ticketID = Console.ReadLine();
-                        
-                        Console.WriteLine("Enter ticket summary: ");
-                        summary = Console.ReadLine();
+            //split ticket ID # so it autogenerates
+            StreamReader sr = new StreamReader(file);
+            string ticketIDstring = File.ReadLines(file).Last().Split(',')[0];
+            int ticketIDNumber;
+            if (!int.TryParse(ticketIDstring, out ticketIDNumber)) {
+                Console.WriteLine("Error");
+            } else {
+                ticketIDNumber += 1;
 
-                        Console.WriteLine("Enter ticket status: ");
-                        status = Console.ReadLine();
+                Console.WriteLine(ticketIDNumber);
+            }
 
-                        Console.WriteLine("Enter ticket priority: ");
-                        priority = Console.ReadLine();
+           
+ 
 
-                        Console.WriteLine("Enter ticket submitter: ");
-                        submitter = Console.ReadLine();
+            StreamWriter sw = new StreamWriter(file);
+            do
+            {               
+                Console.WriteLine("Enter ticket ID: ");
+                ticketID = Console.ReadLine();
 
-                        Console.WriteLine("Who is assigned ticket?: ");
-                        assigned = Console.ReadLine();
+                Console.WriteLine("Enter ticket summary: ");
+                summary = Console.ReadLine();
 
-                        Console.WriteLine("Who is watching ticket?: ");
-                        watching = Console.ReadLine();
+                Console.WriteLine("Enter ticket status: ");
+                status = Console.ReadLine();
 
-                        sw.WriteLine(ticketID + "," + summary + "," + status + "," + priority + "," + submitter + "," + assigned + "," + watching);
+                Console.WriteLine("Enter ticket priority: ");
+                priority = Console.ReadLine();
 
-                        Console.WriteLine("Would you like to add a ticket? (y/n)");
-                        addTicket = Console.ReadLine();
+                Console.WriteLine("Enter ticket submitter: ");
+                submitter = Console.ReadLine();
 
-                    } while(addTicket == "y");
+                Console.WriteLine("Who is assigned ticket?: ");
+                assigned = Console.ReadLine();
 
-                    sw.Close();
+                Console.WriteLine("Who is watching ticket?: ");
+                watching = Console.ReadLine();
 
-            //     }
-            // }
+                sw.WriteLine(ticketID + "," + summary + "," + status + "," + priority + "," + submitter + "," + assigned + "," + watching);
+
+                Console.WriteLine("Would you like to add a ticket? (y/n)");
+                addTicket = Console.ReadLine();
+
+            } while (addTicket == "y");
+
+            sw.Close();
 
         }
     }
