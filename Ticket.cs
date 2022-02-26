2bb4560b7b960924d4db741cf4dfa6ea04370c75
Ashley Stanley <astanley5@my.wctc.edu>
2022-02-26 10:56:20 -0600

    Constructor for Ticket

--- Ticket.cs
+++ Ticket.cs
@@ -5,12 +5,23 @@ namespace Week6Assignment
 {
     public class Ticket
     {
-            public string ticketID { get; set; }
+            public int ticketID { get; set; }
             public string summary { get; set; }
             public string status { get; set; }
             public string priority { get; set; }
             public string submitter { get; set; }
             public string assigned { get; set; }
             public string watching { get; set; }
+
+            public Ticket(int ticketID, string summary, string status, string priority, string submitter, string assigned, string watching)
+            {
+                ticketID = ticketID;
+                summary - summary;
+                status = status;
+                priority = priority;
+                submitter = submitter;
+                assigned - assigned;
+                watching = watching;
+            }
     }
 }
