27d78c500d8cdbadf57670b20413a8850d944f06
Ashley Stanley <astanley5@my.wctc.edu>
2022-03-21 15:00:58 -0500

    added search parameters

--- Program.cs
+++ Program.cs
@@ -18,6 +18,62 @@ namespace Week6Assignment
             string addTicket;
             string searchTickets;
 
+            List<Ticket> Tickets = new List<Ticket>();
+            StreamReader sr = new StreamReader("Bug.csv");
+            while (!sr.EndOfStream){
+                string line = sr.ReadLine();
+                string[] bugDetails = line.Split(',');
+
+                Bug bug = new Bug(UInt16.Parse(bugDetails[0]));
+                bug.summary = bugDetails[1];
+                bug.status = bugDetails[2];
+                bug.priority = bugDetails[3];
+                bug.submitter = bugDetails[4];
+                bug.assigned = bugDetails[5];
+                bug.watching = bugDetails[6];
+                bug.severity = bugDetails[7];                                                                
+
+                Tickets.Add(bug);
+            }
+            sr.Close();
+            sr = new StreamReader("Task.csv");
+            while (!sr.EndOfStream){
+                string line = sr.ReadLine();
+                string[] taskDetails = line.Split(',');
+
+                Task task = new Task(UInt16.Parse(taskDetails[0]));
+                task.summary = taskDetails[1];
+                task.status = taskDetails[2];
+                task.priority = taskDetails[3];
+                task.submitter = taskDetails[4];
+                task.assigned = taskDetails[5];
+                task.watching = taskDetails[6];
+                task.projectName = taskDetails[7];                                                                
+                task.dueDate = taskDetails[7];                                                                
+
+                Tickets.Add(task);
+            }
+            sr.Close();
+            sr = new StreamReader("Enhancements.csv");
+            while (!sr.EndOfStream){
+                string line = sr.ReadLine();
+                string[] enhancementsDetails = line.Split(',');
+
+                Enhancements enhancements = new Enhancements(UInt16.Parse(enhancementsDetails[0]));
+                enhancements.summary = enhancementsDetails[1];
+                enhancements.status = enhancementsDetails[2];
+                enhancements.priority = enhancementsDetails[3];
+                enhancements.submitter = enhancementsDetails[4];
+                enhancements.assigned = enhancementsDetails[5];
+                enhancements.watching = enhancementsDetails[6];
+                enhancements.software = enhancementsDetails[7];                                                                
+                enhancements.cost = enhancementsDetails[7];
+                enhancements.reason = enhancementsDetails[8];                                                                
+                enhancements.estimate = enhancementsDetails[9];                                                                
+                                                                
+                Tickets.Add(enhancements);
+            }
+            sr.Close();
 
             Console.WriteLine("Ticketing System Application\n");
 
@@ -34,7 +90,7 @@ namespace Week6Assignment
                     if (ticketType == "1")
                     {
                         //bug ticket
-                        StreamReader sr = new StreamReader(bugFile);
+                        sr = new StreamReader(bugFile);
                         string last_line = File.ReadLines(bugFile).LastOrDefault();
                         int ticketIDNumber = 1;
                         if (!string.IsNullOrEmpty(last_line))
@@ -60,7 +116,7 @@ namespace Week6Assignment
                     else if (ticketType == "2")
                     {
                         //enhancement ticket
-                        StreamReader sr = new StreamReader(enhancementsFile);
+                        sr = new StreamReader(enhancementsFile);
                         string last_line = File.ReadLines(enhancementsFile).LastOrDefault();
                         int ticketIDNumber = 1;
                         if (!string.IsNullOrEmpty(last_line))
@@ -85,7 +141,7 @@ namespace Week6Assignment
                     else if (ticketType == "3")
                     {
                         //task ticket
-                        StreamReader sr = new StreamReader(taskFile);
+                        sr = new StreamReader(taskFile);
                         string last_line = File.ReadLines(taskFile).LastOrDefault();
                         int ticketIDNumber = 1;
                         if (!string.IsNullOrEmpty(last_line))
@@ -117,7 +173,14 @@ namespace Week6Assignment
             Console.WriteLine("Would you like to search tickets (y/n)? ");
             searchTickets = Console.ReadLine().ToLower();
             if(searchTickets == "y"){
+                //??first enter options to search based on status, priority, submitter?
                 
+                //then enter keyword for search for each ticket type?
+                Console.WriteLine("Enter search keyword(s): ");
+                var ticketSearch = Console.ReadLine();
+
+                var tickets = Tickets.Where(m => m.status.Contains(ticketSearch, StringComparison.OrdinalIgnoreCase));
+                Console.WriteLine(tickets.Count());
             }
 
         }
